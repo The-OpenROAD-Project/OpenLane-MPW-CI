c64ca413ddb239fd49677a550875a1443b7e3edc
vijayank88 <paruthi143@gmail.com>
2022-02-22 02:50:45 +0000

    OL Tag updated to 2022.02.21_02.33.47
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/soric_soc/config.tcl
+++ scripts/soric_soc/config.tcl
@@ -1,6 +1,5 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "[pwd]/caravel"
 set ::env(DESIGN_IS_CORE) 1
-set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.3
 set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.5
 source openlane/soric_soc/config.tcl
--- scripts/soric_soc/setup-flow.sh
+++ scripts/soric_soc/setup-flow.sh
@@ -3,5 +3,5 @@ rsync --archive designs/soric_project/. ${design_dir}
 cp -f ./scripts/${design}/config.tcl ${design_dir}
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/soric_soc/config.tcl
 sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/soric_soc/config.tcl
-sed -i '/::env(PL_RESIZER_HOLD_SLACK_MARGIN)/d' ${design_dir}/openlane/soric_soc/config.tcl
+sed -i '/::env(SYNTH_STRATEGY)/d' ${design_dir}/openlane/soric_soc/config.tcl
 sed -i '/::env(GLB_RESIZER_HOLD_SLACK_MARGIN)/d' ${design_dir}/openlane/soric_soc/config.tcl
