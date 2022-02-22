bd105fd97ab4f36888d0bfddf12998c5ae1cb976
vijayank88 <paruthi143@gmail.com>
2022-02-22 12:05:39 +0000

    jacaranda issue fixed
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/jacaranda8/setup-flow.sh
+++ scripts/jacaranda8/setup-flow.sh
@@ -2,3 +2,5 @@
 rsync --archive designs/${design}/. ${design_dir}
 cp -f ./scripts/${design}/config.tcl ${design_dir}
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/computer/config.tcl
+sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/computer/config.tcl
+sed -i '/::env(ROUTING_CORES)/d' ${design_dir}/openlane/computer/config.tcl
