53b75ad95fa53c6184038d5f0b97a12505f7de6a
vijayank88 <paruthi143@gmail.com>
2022-02-22 08:39:09 +0000

    FCNet config.tcl updated
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/FCNet_prj/config.tcl
+++ scripts/FCNet_prj/config.tcl
@@ -4,4 +4,5 @@ set ::env(DESIGN_IS_CORE) 1
 set ::env(CELL_PAD) 2
 set ::env(FP_CORE_UTIL) 10
 set ::env(PL_TARGET_DENSITY) 0.12
+set ::env(QUIT_ON_MAGIC_DRC) 0
 source openlane/user_proj_example/config.tcl
