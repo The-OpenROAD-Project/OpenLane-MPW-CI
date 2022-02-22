c5cc5424075020330051679142d611132d91a6f7
vijayank88 <paruthi143@gmail.com>
2022-02-22 16:02:53 +0000

    soric_soc hold issue fixed
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/soric_soc/config.tcl
+++ scripts/soric_soc/config.tcl
@@ -2,4 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "[pwd]/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.5
+set ::env(QUIT_ON_HOLD_VIOLATIONS) 0
 source openlane/soric_soc/config.tcl
