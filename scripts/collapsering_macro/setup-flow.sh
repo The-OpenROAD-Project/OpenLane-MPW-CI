a3074cc5d87c6cde01511634e235352d337951f0
vijayank88 <paruthi143@gmail.com>
2022-02-24 11:29:51 +0000

    mpw3 desings added to CI
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

---
+++ scripts/collapsering_macro/setup-flow.sh
@@ -0,0 +1,5 @@
+# copy config files to design dir
+rsync --archive designs/randsack/. ${design_dir}
+cp -f ./scripts/${design}/config.tcl ${design_dir}
+sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/collapsering_macro/config.tcl
+sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/collapsering_macro/config.tcl
