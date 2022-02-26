6d3d65b88099ecbea0028ce427b443825969bbb8
vijayank88 <paruthi143@gmail.com>
2022-02-26 06:13:55 +0000

    mpw2 designs added to CI
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/crypto_core/setup-flow.sh
+++ scripts/crypto_core/setup-flow.sh
@@ -4,4 +4,4 @@ cp -f ./scripts/${design}/config.tcl ${design_dir}
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/crypto_core/config.tcl
 sed -i '/::env(ROUTING_CORES)/d' ${design_dir}/openlane/crypto_core/config.tcl
 sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/crypto_core/config.tcl
-#sed -i '/::env(SYNTH_STRATEGY)/d' ${design_dir}/openlane/crypto_core/config.tcl
+sed -i '/::env(SYNTH_STRATEGY)/d' ${design_dir}/openlane/crypto_core/config.tcl
--- scripts/soric_soc/config.tcl
+++ scripts/soric_soc/config.tcl
@@ -1,4 +1,5 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "[pwd]/caravel"
 set ::env(DESIGN_IS_CORE) 1
+set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) 1
 source openlane/soric_soc/config.tcl
