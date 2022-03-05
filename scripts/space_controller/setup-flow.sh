768649a19a2f7f1ed5255d8a0ee326c77b4f3eea
vijayank88 <paruthi143@gmail.com>
2022-03-05 07:36:49 +0000

    OL tag updated to 2022.03.03_02.36.44
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/space_controller/setup-flow.sh
+++ scripts/space_controller/setup-flow.sh
@@ -4,7 +4,3 @@ cp -f ./scripts/${design}/config.tcl ${design_dir}
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/user_proj_example/config.tcl
 sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/user_proj_example/config.tcl
 sed -i '/::env(ROUTING_CORES)/d' ${design_dir}/openlane/user_proj_example/config.tcl
-sed -i '/::env(PL_RESIZER_HOLD_SLACK_MARGIN)/d' ${design_dir}/openlane/user_proj_example/config.tcl
-sed -i '/::env(PL_RESIZER_MAX_SLEW_MARGIN)/d' ${design_dir}/openlane/user_proj_example/config.tcl
-sed -i '/::env(PL_RESIZER_MAX_CAP_MARGIN)/d' ${design_dir}/openlane/user_proj_example/config.tcl
-sed -i '/::env(PL_RESIZER_HOLD_MAX_BUFFER_PERCENT)/d' ${design_dir}/openlane/user_proj_example/config.tcl
