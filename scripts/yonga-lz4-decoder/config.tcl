0273f07fdb9b5bf181a868f0a9ad39d15e815660
vijayank88 <paruthi143@gmail.com>
2022-03-07 08:00:14 +0000

    yonga and soric HOLD_MARGIN updated
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/yonga-lz4-decoder/config.tcl
+++ scripts/yonga-lz4-decoder/config.tcl
@@ -2,4 +2,6 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "[pwd]/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(CELL_PAD) 2
+set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) 1
+set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.3
 source openlane/user_proj_example/config.tcl
