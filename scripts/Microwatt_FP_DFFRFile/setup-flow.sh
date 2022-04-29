e419d95cf7e68b61ba9cfed145ee6f0eae4b46b4
vijayank88 <paruthi143@gmail.com>
2022-04-29 11:10:36 +0000

    MPW5 designs added
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

---
+++ scripts/Microwatt_FP_DFFRFile/setup-flow.sh
@@ -0,0 +1,5 @@
+# copy config files to design dir
+sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
+sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
+sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
+sed -i '/::env(FP_PDN_CORE_RING)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
---
+++ scripts/rdq_ycr4_iconnect/config.tcl
@@ -0,0 +1,6 @@
+cd [file dirname [file normalize [info script]]]
+set ::env(CARAVEL_ROOT) "[pwd]/caravel"
+set ::env(DESIGN_IS_CORE) 1
+set ::env(FP_IO_MIN_DISTANCE) 2
+set ::env(GLB_RT_ADJUSTMENT) "0.15"
+source openlane/ycr4_iconnect/config.tcl
