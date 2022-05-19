65415450e6378143f79ab4da436d98192fb8a55b
vijayank88 <paruthi143@gmail.com>
2022-05-19 10:13:20 +0000

    OL tag updated to 2022.05.18_02.12.32 from 2022.05.17_01.52.42`
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/Microwatt_FP_DFFRFile/setup-flow.sh
+++ scripts/Microwatt_FP_DFFRFile/setup-flow.sh
@@ -3,3 +3,4 @@ sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/c
 sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
 sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
 sed -i '/::env(FP_PDN_CORE_RING)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
+sed -i '/::env(ECO_ENABLE)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
--- scripts/asicle/setup-flow.sh
+++ scripts/asicle/setup-flow.sh
@@ -2,3 +2,4 @@
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/user_project/config.tcl
 sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/user_project/config.tcl
 sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/user_project/config.tcl
+sed -i '/::env(FP_CORE_UTIL)/d' ${design_dir}/openlane/user_project/config.tcl
