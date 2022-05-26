590c4d84b903ca3b57b3244702299b7167d6aa56
vijayank88 <paruthi143@gmail.com>
2022-05-26 13:14:24 +0000

    production CI list updated
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/ks-guitar-2s/setup-flow.sh
+++ scripts/ks-guitar-2s/setup-flow.sh
@@ -1,2 +1,5 @@
 # copy config files to design dir
 gzip -d ${design_dir}/gds/user_project.gds.gz
+sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/user_project_wrapper/config.tcl
+sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/user_project_wrapper/config.tcl
+sed -i '/::env(MACRO_PLACEMENT_CFG)/d' ${design_dir}/openlane/user_project_wrapper/config.tcl
--- scripts/mpw5_prga/config.tcl
+++ scripts/mpw5_prga/config.tcl
@@ -1,4 +1,5 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "[pwd]/caravel"
 set ::env(BASE_SDC_FILE) "[pwd]/openlane/prga/constraint.sdc"
+set ::env(PL_ROUTABILITY_DRIVEN) 0
 source openlane/prga/config.tcl
