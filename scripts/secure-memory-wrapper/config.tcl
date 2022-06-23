b1c5ee66282f3098d6caf5a342e9187fdfad8a41
vijayan <85787913+vijayank88@users.noreply.github.com>
2022-06-23 15:06:42 +0530

    Update OpenLane stable tag ->2022.06.22_01.42.47 (#51)
    * Update OpenLane stable tag ->2022.06.22_01.42.47
    
    Signed-off-by: vijayank88 <paruthi143@gmail.com>
    
    * fix for sram test
    
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/secure-memory-wrapper/config.tcl
+++ scripts/secure-memory-wrapper/config.tcl
@@ -1,3 +1,5 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "[pwd]/caravel"
+#1152 gh issue
+set ::env(QUIT_ON_MAGIC_DRC) "0"
 source openlane/user_project_wrapper/config.tcl
