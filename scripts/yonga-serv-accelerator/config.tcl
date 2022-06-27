37559669ab905b45ed5a6d16e9ca3b89ab550fa5
vijayank88 <paruthi143@gmail.com>
2022-06-27 10:15:57 +0000

    yonga-serv drc error fixed
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/yonga-serv-accelerator/config.tcl
+++ scripts/yonga-serv-accelerator/config.tcl
@@ -1,4 +1,4 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "[pwd]/caravel"
-set ::env(PL_ROUTABILITY_DRIVEN) 0
+set ::env(GLB_RT_ADJUSTMENT) {0.2}
 source openlane/user_project_wrapper/config.tcl
