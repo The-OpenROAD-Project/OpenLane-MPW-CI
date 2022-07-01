ab73d54d8a45a7ce2996a6caed15b44cb466098c
vijayank88 <paruthi143@gmail.com>
2022-07-01 13:01:32 +0000

    mpw6 designs added
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

---
+++ scripts/rvj1-caravel-soc/config.tcl
@@ -0,0 +1,5 @@
+cd [file dirname [file normalize [info script]]]
+set ::env(CARAVEL_ROOT) "[pwd]/caravel"
+set ::env(DESIGN_IS_CORE) 1
+set ::env(FP_IO_UNMATCHED_ERROR) 0
+source openlane/rvj1_caravel_soc/config.tcl
