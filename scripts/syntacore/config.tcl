17c43539089092f29c161ce0fdc7eb59e984e4ba
vijayank88 <paruthi143@gmail.com>
2022-07-27 07:02:57 +0000

    CELL_PAD updated to DPL_CELL_PADDING
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/syntacore/config.tcl
+++ scripts/syntacore/config.tcl
@@ -1,6 +1,7 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
-set ::env(CELL_PAD) 1
+set ::env(DPL_CELL_PADDING) 2
 set ::env(PL_ROUTABILITY_DRIVEN) 0
 set ::env(PL_TARGET_DENSITY) "0.52"
+set ::env(GRT_ADJUSTMENT) {0.2}
 source $::env(DESIGN_DIR)/openlane/syntacore/config.tcl
