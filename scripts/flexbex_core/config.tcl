a8580107bc8758a4f959097e2217b8f6a65110e5
vijayank88 <paruthi143@gmail.com>
2022-07-29 07:45:20 +0000

    magic drc issue fixed
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/flexbex_core/config.tcl
+++ scripts/flexbex_core/config.tcl
@@ -2,4 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(SYNTH_STRATEGY) "DELAY 1"
+set ::env(DPL_CELL_PADDING) {1}
 source $::env(DESIGN_DIR)/openlane/flexbex_core/config.tcl
