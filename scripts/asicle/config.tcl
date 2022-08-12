110eb2439992ba5c34feed374797507f8a4b2eaf
vijayank88 <paruthi143@gmail.com>
2022-08-12 10:35:13 +0000

    unstable small fixing
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/asicle/config.tcl
+++ scripts/asicle/config.tcl
@@ -1,9 +1,9 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
-set ::env(FP_CORE_UTIL) 0.01
-set ::env(PL_TARGET_DENSITY) 0.15
-set ::env(DPL_CELL_PADDING) 1
+set ::env(FP_CORE_UTIL) 10
+set ::env(PL_TARGET_DENSITY) "0.15"
+set ::env(DPL_CELL_PADDING) 2
 set ::env(GRT_ADJUSTMENT) "0.15"
 set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) 0
 source $::env(DESIGN_DIR)/openlane/user_project/config.tcl
--- scripts/azadi_soc/config.tcl
+++ scripts/azadi_soc/config.tcl
@@ -1,3 +1,5 @@
 cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
+set ::env(FP_CORE_UTIL) "20"
+set ::env(PL_TARGET_DENSITY) "0.25"
 source $::env(DESIGN_DIR)/openlane/user_project_wrapper/config.tcl
