4e79802cf959affec2968da4f55ce9bd763af10e
vijayank88 <paruthi143@gmail.com>
2022-08-19 07:35:52 +0000

    vsdbaby_wrapper updated for magic fail
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/vsdbabysoc_wrapper/config.tcl
+++ scripts/vsdbabysoc_wrapper/config.tcl
@@ -4,4 +4,5 @@ set ::env(FP_PDN_MACRO_HOOKS) "\
 	dac vccd1 vssd1 vccd1 vssd1, \
 	mprj vccd1 vssd1 vccd1 vssd1, \
 	pll vccd1 vssd1 vccd1 vssd1"
+set ::env(QUIT_ON_MAGIC_DRC) 0
 source $::env(DESIGN_DIR)/openlane/user_project_wrapper/config.tcl
