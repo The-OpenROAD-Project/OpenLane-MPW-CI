35dfef93e766c53b157a6c183f3a9c84dfd32bff
vijayank88 <paruthi143@gmail.com>
2022-09-07 07:37:58 +0000

    signoff check disabled
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/rdq_uart_i2cm_usb_spi_top/config.tcl
+++ scripts/rdq_uart_i2cm_usb_spi_top/config.tcl
@@ -2,4 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(PL_TARGET_DENSITY) "0.47"
+set ::env(DPL_CELL_PADDING) 2
 source $::env(DESIGN_DIR)/openlane/uart_i2cm_usb_spi_top/config.tcl
