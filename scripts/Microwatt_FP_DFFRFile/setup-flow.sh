1bd4e5231817993e9acc1dfeaa5887c49ac89626
vijayank88 <paruthi143@gmail.com>
2022-09-09 09:03:48 +0000

    SYNTH_CLOCK_UNCERTAINITY -> SYNTH_CLOCK_UNCERTAINTY
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/Microwatt_FP_DFFRFile/setup-flow.sh
+++ scripts/Microwatt_FP_DFFRFile/setup-flow.sh
@@ -4,3 +4,4 @@ sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/
 sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
 sed -i '/::env(FP_PDN_CORE_RING)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
 sed -i '/::env(ECO_ENABLE)/d' ${design_dir}/openlane/Microwatt_FP_DFFRFile/config.tcl
+sed -i 's/SYNTH_CLOCK_UNCERTAINITY/SYNTH_CLOCK_UNCERTAINTY/g' ${design_dir}/openlane/Microwatt_FP_DFFRFile/base.sdc
