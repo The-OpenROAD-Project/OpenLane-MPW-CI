ae2771e993d5a7fdbc3442bcf30276dc58eace9f
vijayank88 <paruthi143@gmail.com>
2022-09-16 09:50:48 +0000

    fix DPL issue
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/fct_iot_biquad/config.tcl
+++ scripts/fct_iot_biquad/config.tcl
@@ -2,4 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(FP_IO_UNMATCHED_ERROR) 0
+set ::env(DPL_CELL_PADDING) 3
 source $::env(DESIGN_DIR)/openlane/biquad/config.tcl
--- scripts/qf_mkLanaiFrontend/config.tcl
+++ scripts/qf_mkLanaiFrontend/config.tcl
@@ -2,4 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(GRT_ADJUSTMENT) "0.2"
+set ::env(DPL_CELL_PADDING) 3
 source $::env(DESIGN_DIR)/openlane/mkLanaiFrontend/config.tcl
