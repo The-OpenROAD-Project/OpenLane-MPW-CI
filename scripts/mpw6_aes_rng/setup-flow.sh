75f217a7e1378f596db2eddc06ac4149f78ae630
vijayank88 <paruthi143@gmail.com>
2022-10-27 13:21:49 +0000

    design update
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/mpw6_aes_rng/setup-flow.sh
+++ scripts/mpw6_aes_rng/setup-flow.sh
@@ -1,4 +1,5 @@
 # copy config files to design dir
+sed -i '99,112d' ${design_dir}/openlane/user_proj_example/config.tcl
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/user_proj_example/config.tcl
 sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/user_proj_example/config.tcl
 sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/user_proj_example/config.tcl
