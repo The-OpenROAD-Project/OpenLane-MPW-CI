ad052e8a50bfdaef3860d79494a6d98d0343e109
vijayank88 <paruthi143@gmail.com>
2023-01-02 15:17:54 +0000

    ppcpu grt fix
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/ppcpu_dcache/setup-flow.sh
+++ scripts/ppcpu_dcache/setup-flow.sh
@@ -1,8 +1,10 @@
 # copy config files to design dir
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/dcache/config.tcl
-sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/dcache/config.tcl
 sed -i '/::env(ROUTING_CORES)/d' ${design_dir}/openlane/dcache/config.tcl
 sed -i '/::env(MACRO_PLACEMENT_CFG)/d' ${design_dir}/openlane/dcache/config.tcl
+sed -i '/::env(FP_SIZING)/d' ${design_dir}/openlane/dcache/config.tcl
+sed -i '/::env(DIE_AREA)/d' ${design_dir}/openlane/dcache/config.tcl
+sed -i '/::env(CLOCK_PERIOD)/d' ${design_dir}/openlane/dcache/config.tcl
 sed -i 's/\$::env(DESIGN_DIR)/\$script_dir/g' ${design_dir}/openlane/dcache/config.tcl
 sed -i 's/\$script_dir\/..\/../\$::env(DESIGN_DIR)/g' ${design_dir}/openlane/dcache/config.tcl
 sed -i 's/\$script_dir\/pin/\$::env(DESIGN_DIR)\/openlane\/dcache\/pin/g' ${design_dir}/openlane/dcache/config.tcl
