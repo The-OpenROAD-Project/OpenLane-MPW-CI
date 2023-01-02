44badd8c9df435d31eda71a89cda7478dbe26b2b
vijayank88 <paruthi143@gmail.com>
2023-01-02 15:18:39 +0000

    ycr4_iconnect grt fix
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/rdQ2_ycr4_iconnect/config.tcl
+++ scripts/rdQ2_ycr4_iconnect/config.tcl
@@ -2,4 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(FP_IO_MIN_DISTANCE) 2
+set ::env(GRT_ADJUSTMENT) 0.1
 source $::env(DESIGN_DIR)/openlane/ycr4_iconnect/config.tcl
