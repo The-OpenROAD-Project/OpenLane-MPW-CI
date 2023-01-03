4c74f1aa76872b3ccd83dd452ca5750f97f96a71
vijayan <85787913+vijayank88@users.noreply.github.com>
2023-01-03 16:58:52 +0530

    GRT issue fix (#132)
    * patmos grt fix
    
    Signed-off-by: vijayank88 <paruthi143@gmail.com>
    
    * ppcpu grt fix
    
    Signed-off-by: vijayank88 <paruthi143@gmail.com>
    
    * ycr4_iconnect grt fix
    
    Signed-off-by: vijayank88 <paruthi143@gmail.com>
    
    * update CI list
    
    Signed-off-by: vijayank88 <paruthi143@gmail.com>
    
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/ppcpu_dcache/config.tcl
+++ scripts/ppcpu_dcache/config.tcl
@@ -2,5 +2,8 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(FP_IO_UNMATCHED_ERROR) 0
+set ::env(FP_CORE_UTIL) 25
 set ::env(SYNTH_STRATEGY) "DELAY 4"
+set ::env(CLOCK_PERIOD) "12"
+set ::env(IO_PCT) "0.28"
 source $::env(DESIGN_DIR)/openlane/dcache/config.tcl
