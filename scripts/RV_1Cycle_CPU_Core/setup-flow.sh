d064b747f0276dc540893c2239448c7f0099bbbf
vijayan <85787913+vijayank88@users.noreply.github.com>
2023-05-26 19:06:10 +0530

    OpenLane tag update (#151)

--- scripts/RV_1Cycle_CPU_Core/setup-flow.sh
+++ scripts/RV_1Cycle_CPU_Core/setup-flow.sh
@@ -2,6 +2,6 @@
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/RISC_V/config.tcl
 sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/RISC_V/config.tcl
 sed -i '/::env(ROUTING_CORES)/d' ${design_dir}/openlane/RISC_V/config.tcl
-sed -i 's/GLB_RT/GRT/g' ${design_dir}/openlane/RISC_V/config.tcl
+sed -i '/::env(GLB_RT_ADJUSTMENT)/d' ${design_dir}/openlane/RISC_V/config.tcl
 sed -i 's/\$script_dir\/..\/../\$::env(DESIGN_DIR)/g' ${design_dir}/openlane/RISC_V/config.tcl
 sed -i 's/\$script_dir\/pin/\$::env(DESIGN_DIR)\/openlane\/RISC_V\/pin/g' ${design_dir}/openlane/RISC_V/config.tcl
