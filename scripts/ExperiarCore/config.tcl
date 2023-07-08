1653791fb917ae79cb75c35508b93a109a3d28c9
vijayank88 <paruthi143@gmail.com>
2023-07-08 09:54:15 +0000

    Update OpenLane tag-->2023.07.07
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/ExperiarCore/config.tcl
+++ scripts/ExperiarCore/config.tcl
@@ -2,4 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(FP_IO_UNMATCHED_ERROR) 0
+set ::env(QUIT_ON_SYNTH_CHECKS) 0
 source $::env(DESIGN_DIR)/openlane/ExperiarCore/config.tcl
