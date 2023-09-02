f1554e5a0b0362344013929b2a111914527b4776
vijayan <85787913+vijayank88@users.noreply.github.com>
2023-09-03 02:30:12 +0530

    fix linter error (#185)
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/fossiAES/setup-flow.sh
+++ scripts/fossiAES/setup-flow.sh
@@ -2,4 +2,5 @@
 sed -i '/::env(DESIGN_IS_CORE)/d' ${design_dir}/openlane/aes/config.tcl
 sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/aes/config.tcl
 sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/aes/config.tcl
+sed -i '/::env(DIODE_INSERTION_STRATEGY)/d' ${design_dir}/openlane/aes/config.tcl
 sed -i 's/GLB_RT/GRT/g' ${design_dir}/openlane/aes/config.tcl
