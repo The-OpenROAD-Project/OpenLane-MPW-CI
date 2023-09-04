95ae7f01d2645181bd20ee2624decb792fa7f1f7
vijayan <85787913+vijayank88@users.noreply.github.com>
2023-09-04 16:49:31 +0530

    fix fossiAES linter error (#186)
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- scripts/fossiAES/config.tcl
+++ scripts/fossiAES/config.tcl
@@ -2,6 +2,5 @@ cd [file dirname [file normalize [info script]]]
 set ::env(CARAVEL_ROOT) "$::env(DESIGN_DIR)/caravel"
 set ::env(DESIGN_IS_CORE) 1
 set ::env(FP_IO_UNMATCHED_ERROR) 0
-set ::env(QUIT_ON_LINTER_ERRORS) 0
-set ::env(QUIT_ON_SYNTH_CHECKS) 0
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/verilog/rtl/aes/generated/*.v]
 source $::env(DESIGN_DIR)/openlane/aes/config.tcl
--- scripts/fossiAES/setup-flow.sh
+++ scripts/fossiAES/setup-flow.sh
@@ -4,3 +4,4 @@ sed -i '/::env(GLB_RT_MAXLAYER)/d' ${design_dir}/openlane/aes/config.tcl
 sed -i '/::env(RT_MAX_LAYER)/d' ${design_dir}/openlane/aes/config.tcl
 sed -i '/::env(DIODE_INSERTION_STRATEGY)/d' ${design_dir}/openlane/aes/config.tcl
 sed -i 's/GLB_RT/GRT/g' ${design_dir}/openlane/aes/config.tcl
+sed -i '24,25d' ${design_dir}/openlane/aes/config.tcl
